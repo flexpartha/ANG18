bfea973d03ddb475ed19b7821ee78a1a4316a0f0
flexpartha <partha.chakra2@gmail.com>
2025-01-26 23:28:06 +0530

    updated code

--- src/app/pages/admin-role/admin-role.component.ts
+++ src/app/pages/admin-role/admin-role.component.ts
@@ -1,22 +1,35 @@
-import { ChangeDetectorRef, Component, Inject, inject } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  Inject,
+  inject,
+  OnInit,
+} from '@angular/core';
 import { Router } from '@angular/router';
 import {
   FactoryserviceService,
   ROLE_BASED_SERVICE,
 } from '../service/factory/factoryservice.service';
+import { Observable } from 'rxjs';
+import { Address } from '../../interfaces/useraddress.type';
+import { UserAddressService } from '../service/user-address.service';
+import { AsyncPipe, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-admin-role',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe, NgFor],
   templateUrl: './admin-role.component.html',
   styleUrl: './admin-role.component.scss',
 })
-export class AdminRoleComponent {
+export class AdminRoleComponent implements OnInit {
   message: string;
 
   #router = inject(Router);
 
+  userAddress$!: Observable<Address[]>;
+
+  userservice = inject(UserAddressService);
   // constructor(
   //   @Inject(ROLE_BASED_SERVICE) private roleBasedService: any
   // ) //private cdr: ChangeDetectorRef
@@ -30,11 +43,18 @@ export class AdminRoleComponent {
     this.message = service.getData();
   }
 
+  ngOnInit(): void {
+    this.userAddress$ = this.userservice.getAllUserType();
+  }
   logout() {
     localStorage.removeItem('userRole');
     // this.#router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
     //   this.#router.navigate(['/login']); // Reloads the login route
     // });
     this.#router.navigate(['/login']);
+  }
+
+  trackByIndex(index: number): number {
+    return index;
   }
 }
--- src/app/pages/service/user/user.service.ts
+++ src/app/pages/service/user/user.service.ts
@@ -1,12 +1,31 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { catchError, filter, map, Observable, throwError } from 'rxjs';
+import { Address, UserAddress } from '../../../interfaces/useraddress.type';
+import { HttpClient } from '@angular/common/http';
+
+const apiUrl = 'https://jsonplaceholder.typicode.com/users';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
+  private _httClient = inject(HttpClient);
   constructor() {}
 
   getData(): string {
     return 'User Service: User data loaded.';
+  }
+
+  getAllUserType(): Observable<Address[]> {
+    return this._httClient.get<UserAddress[]>(apiUrl).pipe(
+      map((response) => {
+        return response.map((userAddress) => userAddress.address);
+      }),
+      filter((address) => address.length > 0),
+      catchError((error: any) => {
+        console.log(error);
+        return throwError(() => new Error(error));
+      })
+    );
   }
 }
